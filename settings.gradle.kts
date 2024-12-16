f771293006d3423db48ea4459a114ddea2001060
s92075608 <91215788+s92075608@users.noreply.github.com>
2024-12-16 19:24:02 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TaskMonitoringApp_funtastic4"
+include(":app")
+ 
